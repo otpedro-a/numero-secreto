208b4532f34cb728f8d1720f7d1f9745e5c76e4e
Pedro Oliveira <otpedro@hotmail.com>
2025-02-03 18:30:36 -0300

    Aletrando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 listaDeNumerosSorteados = [];
-let limiteDeTentativas = 10;
+let limiteDeTentativas = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemIncial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto 2.0');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemIncial();
